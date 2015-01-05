713a93ab3af1af1e8f9233a1b9e18021483f0f59
Yufengli <hotsauceeminem@gmail.com>
2015-01-05 11:58:44 +0800

    Changed to O(n)

--- TwoSum/TwoSum.cpp
+++ TwoSum/TwoSum.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int> &numbers, int target) {
-	vector<int> res;
-        for (vector<int>::size_type first = 0;first != numbers.size();
-	        first++) {
-	    for (vector<int>::size_type second = 0;second != numbers.size();
-		    second++) {
-		if (numbers[first]+numbers[second] == target) {
-		    res.push_back(first+1);
-		    res.push_back(second+1);
-		}
-	    }
-	}
-	return res;
+        vector<int> res;
+        map<int, int> h_map;
+        //initial h_map;
+        h_map.clear();
+        for (vector<int>::size_type tem = 0;tem != numbers.size();tem++) {
+            h_map[numbers[tem]] = tem;
+        }
+
+        for (vector<int>::size_type first = 0;first != numbers.size();first++) {
+            if (h_map.find(target - numbers[first]) != h_map.end()) {
+                if (first < h_map[target - numbers[first]]) {
+                        res.push_back(first+1);
+                        res.push_back(h_map[target-numbers[first]]+1);
+                } else if (first > h_map[target - numbers[first]]) {
+                    res.push_back(h_map[target-numbers[first]]+1);
+                    res.push_back(first+1);
+                }
+            }
+        }
+        return res;
     }
 };
